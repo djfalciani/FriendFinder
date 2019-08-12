b224e35047314a9cacdf3b997fe745c241cd6607
djfalciani <dfalciani@gmail.com>
2019-08-12 01:37:23 -0400

    added basic friend mock data

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+const friends = [
+    // Friend Example:
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [1,2,3,4,5,1,2,3,4,5]
+    }
+]
+
+module.exports = friends;
