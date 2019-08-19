417457833f16d1873c681b3a0201b60f3d2b6c1f
djfalciani <dfalciani@gmail.com>
2019-08-19 08:27:39 -0400

    wip

--- app/public/assets/javascript/survey.js
+++ app/public/assets/javascript/survey.js
@@ -49,11 +49,22 @@ const renderSurveyQuestions = () => {
 
 // onClick of the Submit Survey Btn...
 $("#postFriend").on("click", function(event) {
+// $(".submit").on("click", function(event) {
     event.preventDefault();
     
     const scores = [];
     let isNaN = false;
     
+    // get form values
+    const surveyName = $("#survey-name").val().trim();
+    const surveyPhoto = $("#survey-photo").val().trim();
+    
+    // validate user has completed survey...
+    if (surveyName === '' || surveyPhoto === '' ) {
+      alert("Please provide name and photo link");
+      return;
+    }
+
     // Whenever a user selects an option we need to push it into an array...
     $('select.custom-select option:selected').each(function() {
         let value = parseInt($(this).val().trim())
@@ -75,23 +86,35 @@ $("#postFriend").on("click", function(event) {
     
     // Post new Friend...
     const newFriend = {
-        name: $("#survey-name").val().trim(),
-        photo: $("#survey-photo").val().trim(),
+        name: surveyName,
+        photo: surveyPhoto,
         scores: scores,
     };
 
-    console.log(newFriend);
+    // DJF - this was throwing an error because I was using a minified version of jQuery...
+    //<!-- full jquery -->
+    //<script src="https://code.jquery.com/jquery.js"></script>
+    // $.post("/api/friends", newFriend, function(data) {
+    //   console.log(data);
+    // });
 
-    $.post("/api/friends", newFriend).then(function(data) {
-        console.log(data);
-        alert("Survey successfully added");
-    });
+    const url = "/api/friends";
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(newFriend),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.json())
+      .then(data => {})
+      .catch(error => console.error("Error:", error));
+
     
-    // $.post("/api/characters", newCharacter)
-    // .then(function(data) {
-    //   console.log("add.html", data);
-    //   alert("Adding character...");
-    // })
+    let matchedFriend = friendMatch(newFriend);
+    if (matchedFriend) {
+      $('#exampleModal').modal('toggle')
+    } 
 
     resetSurvey();
 
@@ -103,5 +126,38 @@ function resetSurvey () {
     $("#survey-photo").val("");
     $('select.custom-select').val("Open this select menu");
 };
+
+// friendMatch - will loop through the Friends arary and try to find a close match. Array is newest created friend...
+function friendMatch(arr) {
+  const totalDiffArr = [];
+  
+  let totalDiff = 0;
+  $.get("/api/friends", function(data) {  
+    data.forEach(element => {
+      dataScore = element.scores;
+      console.log(dataScore);
+      
+      // // DJF - at this point element.scores = friend1.score = [1,2,3,4,5,1,2,3,4,5] we just need to compare to the matched answer in our arr value...
+      for (i=0; i < dataScore.length-1; i++) {
+        let userRes = arr[i];
+        let diff = Math.abs(userRes - dataScore[i]);
+        //totalDiff + diff;
+      }
+      // console.log(totalDiff);
+      // totalDiff now represents the match value for a single comparison. We need to stash that and check the others...
+      
+      // totalDiffArr.push(totalDiff);
+    });
+  });
+  console.log(totalDiff);
+  return false;
+}
+
+function toggleModal() {
+  $('#exampleModal').modal('toggle')
+};
+
+// $("#myModalTest").on('click', (e) => {
+// });
 
 renderSurveyQuestions();
